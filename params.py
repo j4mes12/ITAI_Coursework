da9d76e13853664165f0985335d6920716a99e38
j4mes12 <jamesstephenson@Jamess-Air.cust.communityfibre.co.uk>
2022-03-14 17:19:16 +0000

    initial commit

---
+++ params.py
@@ -0,0 +1,20 @@
+RANDOM_STATE = 73
+
+MODEL_RESPONSE = "rented_bike_count"
+
+MODEL_FEATURES = [
+    "hour",
+    "temperature",
+    "humidity",
+    "wind_speed",
+    "visibility",
+    "dew_point_temperature",
+    "solar_radiation",
+    "rainfall",
+    "snowfall",
+    "holiday",
+    "functioning_day",
+    "seasons_Autumn",
+    "seasons_Spring",
+    "seasons_Summer",
+]

73c840c1e16dd0fbdc4b8a0451a837fbba59b078
James Stephenson <12j4mes@gmail.com>
2022-03-16 08:10:03 +0000

    feature stuff and modelling

--- params.py
+++ params.py
@@ -2,18 +2,31 @@ RANDOM_STATE = 73
 
 MODEL_RESPONSE = "rented_bike_count"
 
+MODEL_FEATURES_WB = [
+    "hour",
+    "temperature",
+    "humidity",
+    "wind_speed",
+    "visibility",
+    "binary_solar_radiation",
+    "binary_rainfall",
+    "binary_snowfall",
+    "holiday",
+    "seasons_Autumn",
+    "seasons_Spring",
+    "seasons_Summer",
+]
+
 MODEL_FEATURES = [
     "hour",
     "temperature",
     "humidity",
     "wind_speed",
     "visibility",
-    "dew_point_temperature",
     "solar_radiation",
     "rainfall",
     "snowfall",
     "holiday",
-    "functioning_day",
     "seasons_Autumn",
     "seasons_Spring",
     "seasons_Summer",
